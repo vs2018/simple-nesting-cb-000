7c6ac491dc34a4d1a3660b9cdab8f6ded2c6c175
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 09:15:36 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,10 +85,10 @@ def adding_matz
         }
      }
      
-     programmer_hash[:yukihiro_matsumoto] = {
-       :known_for => "Ruby",
-       :languages => ["LISP", "C"]
-     }
+    # programmer_hash[:yukihiro_matsumoto] = {
+    #   :known_for => "Ruby",
+    #   :languages => ["LISP", "C"]
+    # }
      
      {yukihiro_matsumoto: {
        :known_for => "Ruby",
